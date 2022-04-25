5759c20d195de4687e1704bd94ab07fb856c55cf
Mickey Katz <mickeykatz12@gmail.com>
2022-04-25 17:39:20 -0400

    Work In Progress: Add Switches to Profile Details

--- equitive_website/src/components/ProfileDetails.js
+++ equitive_website/src/components/ProfileDetails.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Button, Form } from 'react-bootstrap'
 import { Link } from "react-router-dom"
 import { setDoc, collection, doc } from "firebase/firestore"; 
@@ -6,6 +6,12 @@ import {db} from "../firebase"
 import firebase from "firebase/compat/app";
 
 export default function ProfileDetails() {
+
+  const [jobDescriptions, setJobDescriptions] = useState(false)
+  const [sourcing, setSourcing] = useState(false)
+  const [resumes, setResumes] = useState(false)
+  const [interviews, setInterviews] = useState(false)
+  const [hiringDecision, setHiringDecisions] = useState(false)
 
   async function handleSubmit(e) {
     e.preventDefault()
@@ -15,11 +21,7 @@ export default function ProfileDetails() {
         last: "Turing",
         jobTitle: "Software Engineer",
         department: "Engineering",
-        jobDescriptions: 0,
-        sourcing: 0,
-        resumes: 1, 
-        interview: 1,
-        hiringDecision: 1
+        roles: ["resumes", "interview", "hiringDecision"]
       });
     
       console.log("Document written with email: ", firebase.auth().currentUser.email);
@@ -35,11 +37,43 @@ export default function ProfileDetails() {
       <p>What parts of the hiring process are you involved in? Select all that apply*</p>
       <Link to="/dashboard">Finish</Link>
       <Form onSubmit={handleSubmit}>
+        <Form.Check 
+          type="switch"
+          id="custom-switch"
+          label="Defining or Writing Job Descriptions"
+          value={!jobDescriptions}
+          onChange={ (e) => setJobDescriptions( e.target.value )}
+        />
+        <p>
+            {jobDescriptions}
+        </p>
+        <Form.Check 
+          type="switch"
+          id="custom-switch"
+          label="Seeking or Sourcing Talent"
+        />
+        <Form.Check 
+          type="switch"
+          id="custom-switch"
+          label="Collecting and Screening Resumes"
+        />
+        <Form.Check 
+          type="switch"
+          id="custom-switch"
+          label="Conducting Interviews"
+        />
+        <Form.Check 
+          type="switch"
+          id="custom-switch"
+          label="Making Hiring Decisions"
+        />
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
       </Form>
     </Container>
+    
 
   )
 }
