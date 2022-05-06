b788eeadbcebc360578b2af8daad97c5f96d5c30
Grace Lang <graceelang@gmail.com>
2022-05-06 12:44:25 -0400

    add pricing details

--- equitive_website/src/components/Product/Product.js
+++ equitive_website/src/components/Product/Product.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Container, Row, Col, Card, Image } from 'react-bootstrap'
 import "./product.css";
+import {BsStar, BsStarHalf, BsStarFill} from 'react-icons/bs'
 
 export default function Product() {
   return (
     <>
-    <h1 className="text-center mt-3">Our answer to one-size-fits all training modules</h1>
+    <h1 className="text-center mt-5 mb-5 title">Our answer to one-size-fits all training modules</h1>
     <div className = "light-background">
     <Container>
           <Row>
@@ -45,11 +46,12 @@ export default function Product() {
           <Row>
             <Col className="d-flex align-items-center">
               <Container>
+                <h2>Our Answer to one-time modules: continuous training via slackbot</h2>
                 <p><strong>A slack integration, so the D&amp;I training doesn't stop when the modules end. Reminders, check-ins, and quizzes can be setup so your employees refresh their skills seamlessly within their workspace. </strong></p>
               </Container>
             </Col>
             <Col className="d-flex align-items-center justify-content-center">
-              <Image className="img-thumbnail secondary-image mb-5" src={require("./dashboard.png")}
+              <Image className="img-thumbnail secondary-image mb-5" src={require("./slackbot.png")}
                 alt={"people"}/>
             </Col>
           </Row>
@@ -57,16 +59,39 @@ export default function Product() {
         <h1 className="text-center">Pricing</h1>
         <Row>
           <Col>
-          <Card className="mb-5">
-            Price 1
+          <Card className="mb-5 text-center">
+            <h2 className="price mt-3">BASIC</h2>
+            <Col className='text-center mb-3'>
+              <Container>
+              <BsStar size={30} className="text-center"/>
+              </Container>
+            </Col>
+            <p> 1-50 Users</p>
+            <p>$20/mo/user</p>
           </Card>
           </Col>
           <Col>
-          <Card className="mb-5">
-            Price 2</Card></Col>
+          <Card className="mb-5 text-center">
+          <h2 className="price mt-3">START-UP</h2>
+          <Col className='text-center mb-3'>
+              <Container>
+              <BsStarHalf size={30} className="text-center"/>
+              </Container>
+            </Col>
+            <p> 51-150 Users</p>
+            <p>$19/mo/user</p>
+            </Card>
+            </Col>
             <Col>
-            <Card className="mb-5">
-              Price 3
+            <Card className="mb-5 text-center">
+            <h2 className="price mt-3">ENTERPRISE</h2>
+            <Col className='text-center mb-3'>
+              <Container>
+              <BsStarFill size={30} className="text-center"/>
+              </Container>
+            </Col>
+            <p> 151+ Users</p>
+            <p>$18/mo/user</p>
             </Card>
             </Col>
         </Row>
